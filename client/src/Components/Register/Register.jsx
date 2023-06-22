c29c663150276472a9bfbbb6c295631b308939aa
Philippe Baurens <p.baurens@mazarine.com>
2023-06-22 19:31:59 +0200

    Register component post request

--- client/src/Components/Register/Register.jsx
+++ client/src/Components/Register/Register.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+/* eslint-disable no-unused-vars */
+
+import React, { useState } from 'react'
 import '../../App.css'
 import './Register.css'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 // Import our assets
 import video from '../../LoginAssets/video bitcoin.mp4'
@@ -13,7 +16,62 @@ import {BsFillShieldLockFill} from 'react-icons/bs'
 import {AiOutlineSwapRight} from 'react-icons/ai'
 import {MdMarkEmailRead} from 'react-icons/md'
 
+
 const Register = () => {
+
+	const [state, setState] = useState({
+		email: "",
+		username: "",
+		password: ""
+	});
+
+	const handleChange = (e) => {
+		const value = e.target.value;
+		setState({
+		...state,
+		[e.target.name]: value
+		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const userData = {
+		email: state.email,
+		username: state.username,
+		password: state.password
+		};
+
+		axios.post("http://localhost:3002/register", userData).then((response) => {
+		console.log(response.status, response.data);
+	});
+
+
+	// const [email, setEmail] = useState('')
+	// const [userName, setUserName] = useState('')
+	// const [password, setPassword] = useState('')
+
+	// React.useEffect(() => {
+	// 	axios.get(`${baseURL}/register`).then((response) => {
+	// 	setPost(response.data);
+	// 	});
+	// }, []);
+
+	// // Onclick let us get  what user has entered
+
+	// function createUser() {
+	// 	// We shall require Axios to create an API that connects to the server
+	// 	axios.post(baseURL, {
+	// 		// create variable to send to the server through the route
+	// 		Email: email,
+	// 		UserName: userName,
+	// 		Password: password
+	// 	})
+	// 	.then((response) => {
+	// 		setPost(response.data);
+	// 		console.log('User has been created')
+	// 	});
+	}
+
   return (
 	<div className='registerPage flex'>
 		<div className='container flex'>
@@ -35,30 +93,36 @@ const Register = () => {
 					<img src={logo} alt="Logo Image"/>
 					<h3>REGISTER</h3>
 				</div>
-				<form action="" className='form grid'>
+				<form onSubmit={handleSubmit} className='form grid'>
 					<div className="inputDiv">
 						<label htmlFor="email">Email</label>
 						<div className="input flex">
 						<MdMarkEmailRead className='icon'/>
-						<input type="email" id="email" placeholder='Enter Email'/>
+						<input type="email" name="email" id="email" placeholder='Enter Email'
+						value={state.email}
+						onChange={handleChange}/>
 						</div>
 					</div>
 					<div className="inputDiv">
 						<label htmlFor="username">Username</label>
 						<div className="input flex">
 						<FaUserShield className='icon'/>
-						<input type="text" id="username" placeholder='Enter Username'/>
+						<input type="text" name="username" id="username" placeholder='Enter Username'
+						value={state.username}
+						onChange={handleChange}/>
 						</div>
 					</div>
 					<div className="inputDiv">
 						<label htmlFor="password">Password</label>
 						<div className="input flex">
 						<BsFillShieldLockFill className='icon'/>
-						<input type="password" id="password" placeholder='Enter Password'/>
+						<input type="password" name="password" id="password" placeholder='Enter Password'
+						value={state.password}
+						onChange={handleChange}/>
 						</div>
 					</div>
 					<button type="submit" className='btn flex'>
-						<span>Login</span>
+						<span>Register</span>
 						<AiOutlineSwapRight className='icon' />
 					</button>
 
--- server/index.js
+++ server/index.js
@@ -4,8 +4,10 @@ const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
 
-// Run the server
+app.use(express.json())
+app.use(cors())
 
+// Run the server
 app.listen(3002, ()=>{
 	console.log('Server is running on port 3002')
 })
@@ -14,13 +16,32 @@ app.listen(3002, ()=>{
  const db = mysql.createConnection({
 	user: 'root',
 	host: 'localhost',
-	password: '',
+	password: 'root',
 	database: 'cryptodb',
+	port: 8889,
  })
+ console.log('Connection to database ok')
 
 // Create route to the server that will register a new user
+app.post('/register', (req, res)=>{
+	// We need to get variables sent from the form
+	const sentEmail = req.body.Email
+	const sentUserName = req.body.UserName
+	const sentPassword = req.body.Password
+	// Lets create SQL statement to insert the user data to the database table users
+	const SQL = 'INSERT INTO users (email, username, password) VALUES (?,?,?)'
+	// We are going to enter these values through a variable
+	const Values = [sentEmail, sentUserName, sentPassword]
 
-app.post('/register', (req, res) =>{
-	// wen need to get variables sent from the form
+	console.log('Values', Values)
 
+	// Query to execute the sql statement stated above
+	db.query(SQL, Values, (err, results)=>{
+		if(err){
+			res.send(err)
+		} else {
+			console.log('User inserted successfully!')
+			res.send({message: 'User added!'})
+		}
+	})
 })
